c8649acafd8818a5d14b388a7ea901f844772005
Sofi3406 <sofiyasin190@gmail.com>
2025-09-15 13:20:18 -0700

    feat:implement simple arithmetic problem

---
+++ arithmeti_problem.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Print sum
+    print(a + b)
+    
+    # Print difference
+    print(a - b)
+    
+    # Print product
+    print(a * b)
