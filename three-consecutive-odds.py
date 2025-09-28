46c0c7a8ed3de43b884b65aaaba6c40a80aad219
Sofi3406 <sofiyasin190@gmail.com>
2025-09-28 11:36:20 -0700

    feat: added consecutive odd problem

---
+++ three-consecutive-odds.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        counter = 0
+        for num in arr:
+            if num % 2 == 1:  
+                counter += 1  
+                if counter == 3: 
+                    return True
+            else:
+                counter = 0  
+        return False  
