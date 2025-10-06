d3a9fb0e9581caa42cc148f99a5dd32f50b8adce
Sofi3406 <sofiyasin190@gmail.com>
2025-10-05 21:10:33 -0700

    feat:added majority-element problem

---
+++ intersection-of-two-arrays-ii.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        
+        count = Counter(nums1)
+        intersection = []  
+        for num in nums2:
+            if count[num] > 0: 
+                intersection.append(num)  
+                count[num] -= 1  
+        
+        return intersection 
